f5edcf9a41e100d3723d3c774c0a3e3369c94b93
Anton Waryat <waryataw@gmail.com>
2021-04-22 20:33:50 +0300

    27. Learn how to decode a JSON file and fetch data with Swift

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Антон Варят on 22.04.2021.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,37 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Антон Варят on 22.04.2021.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    let coverImages: [CoverImage] = Bundle.main.decode("covers.json")
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(coverImages) { item in
+                Image(item.name)
+                    .resizable()
+                    .scaledToFill()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+            .padding()
+    }
+}
