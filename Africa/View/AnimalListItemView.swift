7f487c816655541b77211ae355e7c5f75a99af2e
Anton Waryat <waryataw@gmail.com>
2021-04-22 21:21:33 +0300

    28. Let’s create a dynamic SwiftUI List using Swift Generics and JSON data

---
+++ Africa/View/AnimalListItemView.swift
@@ -0,0 +1,52 @@
+//
+//  AnimalListItemView.swift
+//  Africa
+//
+//  Created by Антон Варят on 22.04.2021.
+//
+
+import SwiftUI
+
+struct AnimalListItemView: View {
+    // MARK: - PROPERTIES
+    
+    let animal: Animal
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 16) {
+            Image(animal.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 90, height: 90)
+                .clipShape(
+                    RoundedRectangle(cornerRadius: 12)
+                )
+            VStack(alignment: .leading, spacing: 8) {
+                Text(animal.name)
+                    .font(.title3)
+                    .fontWeight(.heavy)
+                    .foregroundColor(.accentColor)
+                
+                Text(animal.headline)
+                    .font(.footnote)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(2)
+                    .padding(.trailing, 8)
+            } //: VSTAK
+        } //: HSTACK
+    }
+}
+
+// MARK: - PREVIEW
+
+struct AnimalListItemView_Previews: PreviewProvider {
+    
+    static let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    static var previews: some View {
+        AnimalListItemView(animal: animals[1])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
