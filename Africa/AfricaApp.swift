a148b27127696fe2cf4bd1b9e389d071239529cb
Anton Waryat <waryataw@gmail.com>
2021-04-22 08:52:09 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Антон Варят on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
