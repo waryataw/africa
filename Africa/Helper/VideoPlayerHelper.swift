cdf2d8150406c337b3169f89a90eedf2d354667a
Anton Waryat <waryataw@gmail.com>
2021-04-27 21:02:18 +0300

    32. Let’s create a Video Player with SwiftUI 2 framework - Part 2

---
+++ Africa/Helper/VideoPlayerHelper.swift
@@ -0,0 +1,20 @@
+//
+//  VideoPlayerHelper.swift
+//  Africa
+//
+//  Created by Антон Варят on 27.04.2021.
+//
+
+import Foundation
+import AVKit
+
+var videoPlayer: AVPlayer?
+
+func playVideo(fileName: String, fileFormat: String) -> AVPlayer {
+    if Bundle.main.url(forResource: fileName, withExtension: fileFormat) != nil {
+        videoPlayer = AVPlayer(url: Bundle.main.url(forResource: fileName, withExtension: fileFormat)!)
+        videoPlayer?.play()
+    }
+    
+    return videoPlayer!
+}
---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,47 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Антон Варят on 27.04.2021.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    var videoSelected: String
+    var videoTitle: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+//                Text(videoTitle)
+            }
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 8)
+                    .padding(.horizontal, 8)
+                , alignment: .topLeading
+            )
+        } //: VSTACK
+        .accentColor(.accentColor)
+        .navigationBarTitle(videoTitle, displayMode: .inline)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+        }
+    }
+}
